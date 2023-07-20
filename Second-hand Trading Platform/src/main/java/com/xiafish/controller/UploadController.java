470e7c74d4c69a341223b795994b2589f3bcd044
黄子安 <9764817+aaaz718@user.noreply.gitee.com>
2023-07-20 19:34:51 +0800

    头像上传bug修复

--- Second-hand Trading Platform/src/main/java/com/xiafish/controller/UploadController.java
+++ Second-hand Trading Platform/src/main/java/com/xiafish/controller/UploadController.java
@@ -6,9 +6,7 @@ import com.xiafish.service.UserService;
 import com.xiafish.util.AliOSSUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,15 +16,17 @@ import java.util.List;
 @Slf4j
 @RestController
 public class UploadController {
+
     @Autowired
     private AliOSSUtils aliOSSUtils;
     @Autowired
     private UserService userService;
     @Autowired
     private GoodsService goodsService;
+
     @PostMapping("/upload")
-    public Result uploadHeadImg(Integer userId, MultipartFile image) throws IOException {
-        log.info("文件上传{}",image);
+    public Result uploadHeadImg(@RequestAttribute("userId")Integer userId, MultipartFile image) throws IOException {
+        log.info("文件上传:{}",image);
         String url= aliOSSUtils.upload(image);
         log.info("文件上传成功，对应的url为:{}",url);
         userService.updateHeadImg(userId,url);
